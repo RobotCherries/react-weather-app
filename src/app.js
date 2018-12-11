a2b1e4089bc58aedb86ebda2e2f61f709bdf2873
RobotCherries <zdip14@gmail.com>
2018-12-11 16:55:01 +0200

    Added basic  React Template

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// Default
+import React from 'react'
+import ReactDOM from 'react-dom'
+// Components
+import App from './components/App'
+// Styles
+import './styles.scss'
+
+
+const root = document.getElementById('app-container')
+
+ReactDOM.render(<App /> , root)
